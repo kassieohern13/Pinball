42813173ed0909d91a57a90132dd1e9b81412d17
obi3333 <123705970+obi3333@users.noreply.github.com>
2024-12-02 20:29:18 -0500

    Quality of life and points system
    What she said

--- Assets/BumperPoints.cs
+++ Assets/BumperPoints.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
 
-public class ScoreKeeper : MonoBehaviour
+
+public class BumperPoints : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; 
     private int score = 0;          
@@ -16,12 +18,18 @@ public class ScoreKeeper : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         
-        if (collision.gameObject.CompareTag("Ball"))
+        if (collision.gameObject.CompareTag("Bumper"))
+        {
+            score += 100;
+            UpdateScoreText();
+        }
+        if (collision.gameObject.CompareTag("Fail"))
         {
             score = 0;
             UpdateScoreText();
         }
     }
+    
 
     private void UpdateScoreText()
     {
